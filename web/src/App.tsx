94d792bb5fb07c8518807ccc56ec64dfc5ab16d9
caique-radam <caiqueradam@hotmail.com>
2024-10-13 21:28:55 -0300

    Crud com tabela de produtos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,35 +1,146 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() { 
+  interface Product {
+    id: string;
+    code: string;
+    name: string;
+    created_at: Date;
+  }
+  
+  const [products, setProducts] = useState<Product[]>([]);
+  const [productCode, setProductCode] = useState<string>('');
+  const [productName, setProductName] = useState<string>('');
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null); // Para edição
+
+  const fetchUrl = 'http://localhost:3000'; // Endereço base das requisições
+
+  // Carrega a lista em primeira instância
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const result = await axios.get(`${fetchUrl}/product`);
+        console.log(result);
+        setProducts(result.data);
+      } catch (error) {
+        console.error('Erro ao buscar produtos: ', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  // Atualiza a lista de produtos
+  async function reloadProductList() {
+    const resultFetch =  await axios.get(`${fetchUrl}/product`);
+    setProducts(resultFetch.data);
+  }
+
+  // Limpar campos e selecionar produto
+  function cleanStates() {    
+    setProductCode('');
+    setProductName('');
+    setSelectedProduct(null);
+  }
+
+
+  const handleSave = async () => {
+    try {
+      if (selectedProduct) {
+        try {
+          const {id, created_at} = selectedProduct;
+
+          await axios.put(`${fetchUrl}/product`, {
+            id: id,
+            code: productCode,
+            name: productName,
+            create_at: created_at
+          });
+        } catch (error) {
+          console.log('Erro ao salvar as alterações!', error);
+        }
+      }
+      else {
+        try {
+          await axios.post(`${fetchUrl}/product`, {
+            code: productCode,
+            name: productName
+          });
+        } catch (error) {
+          console.log('Erro ao salvar o produto', error);
+        }
+      } 
+
+      cleanStates();
+      reloadProductList();
+    } catch (error) {
+      console.log('Erro ao Salvar o produto' + error);
+    }
+  };
+
+  const handleEdit = async (product: Product) => {
+    setProductCode(product.code);
+    setProductName(product.name);
+    setSelectedProduct(product); // Define o produto está sendo editado
+  };
+
+  const handleDelete = async (product: Product) => {
+    try {
+      await axios.delete(`${fetchUrl}/product/${product.id}`);
+      reloadProductList();
+    } catch (error) {
+      console.log('Erro ao deletar o produto' + error);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section>
+        <div className='div-inputs'>
+          <div>
+            <label htmlFor="codeField">Code:</label>
+            <input type="text" value={productCode} onChange={(event) => setProductCode(event.target.value)} />
+          </div>
+          <div>
+            <label htmlFor="nameField">Name:</label>
+            <input type="text" value={productName} onChange={(event) => setProductName(event.target.value)} />
+          </div>
+          <button onClick={handleSave}>
+            {selectedProduct ? 'Atualizar' : 'Salvar'}
+          </button>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Code</th>
+                <th>Name</th>
+                <th>Created_At</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => (
+                <tr key={index}>
+                  <td>{product.id}</td>
+                  <td>{product.code}</td>
+                  <td>{product.name}</td>
+                  <td>{product.created_at}</td>
+                  <td>
+                    <button onClick={() => handleEdit(product)}>Editar</button>
+                    <button onClick={() => handleDelete(product)}>Excluir</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
